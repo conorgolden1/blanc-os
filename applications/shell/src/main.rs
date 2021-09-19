7600771646b4327dbaba0fb1e2b0fe3261e4b45e
goldencm <goldencm203@potsdam.edu>
2021-09-19 10:39:53 -0400

    added template code in shell, TODO: consider using the syscall to a stdlib

--- applications/shell/src/main.rs
+++ applications/shell/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+#![feature(lang_items)]
+#![feature(global_asm)]
+#![allow(dead_code)]
+
+use core::panic::PanicInfo;
+
+global_asm!(include_str!("syscall_interrupts.s"));
+
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+
+}
+
+extern "C" {
+    fn syscall(call_num : u64, param1 : u64, param2 : u64, param3: u64) -> u64;
+}
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
 }
