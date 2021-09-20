e2c2971e4b0a51db10177274d0ef786b7cf6cb6e
goldencm <goldencm203@potsdam.edu>
2021-09-20 10:01:35 -0400

    prints out stack pointer to curr stack in use TODO consider using a different app to isolate helloworld as standalone

--- applications/hello_world/src/main.rs
+++ applications/hello_world/src/main.rs
@@ -2,6 +2,7 @@
 #![no_std]
 #![feature(lang_items)]
 #![feature(global_asm)]
+#![feature(asm)]
 #![allow(dead_code)]
 
 use core::panic::PanicInfo;
@@ -11,11 +12,20 @@ global_asm!(include_str!("syscall_interrupts.s"));
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let hello_world = "hello world";
+    let hello_world = "HELLO WORLD\n";
     let hello_world_ptr = hello_world.as_ptr() as u64;
     let num_bytes = hello_world.as_bytes().len();
-    unsafe { syscall(0, 0, hello_world_ptr ,num_bytes as u64)};
+    let stack_pointer : u64;
+    unsafe {
+        asm!{
+            "mov {}, rbp",
+            out(reg) stack_pointer, 
+         };
+    }
+    use core::ptr::addr_of;
 
+    unsafe { syscall(0, 0, hello_world_ptr ,num_bytes as u64)};
+    unsafe { syscall(0, 0, addr_of!(stack_pointer) as u64, 8)};
     loop {
 
     }
