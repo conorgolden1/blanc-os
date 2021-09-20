88ad076f44c61e8ac9898c849b499f5665f20075
goldencm <goldencm203@potsdam.edu>
2021-09-20 10:03:50 -0400

    rustfmt & unfinished attempting scheduler invoking from timer interrupt

--- crate/interrupts/src/lib.rs
+++ crate/interrupts/src/lib.rs
@@ -6,15 +6,16 @@
 #![feature(asm)]
 
 /// Calls the load IDT function, loading the table into the cpu
-pub fn init_idt() { 
+pub fn init_idt() {
     IDT.load();
 }
 
 use coop::keyboard;
 use coop::mouse;
 use lazy_static::lazy_static;
-use x86_64::structures::idt::InterruptDescriptorTable;
 use printer::{print, println};
+use task::scheduler::Scheduler;
+use x86_64::structures::idt::InterruptDescriptorTable;
 
 pub mod syscall;
 
@@ -47,7 +48,7 @@ lazy_static! {
         idt.divide_error.set_handler_fn(divide_error_handler);
         idt.general_protection_fault.set_handler_fn(general_protection_handler);
         idt.virtualization.set_handler_fn(virtualization_handler);
-    
+
 
         idt
     };
@@ -56,10 +57,10 @@ lazy_static! {
 extern "x86-interrupt" fn syscall(_: InterruptStackFrame) {
     //     unsafe { syscall(4, 5, 6 ,7)};
 
-    let call_num : u64 ;
-    let param1 : u64;
-    let param2 : u64;
-    let param3 : u64;
+    let call_num: u64;
+    let param1: u64;
+    let param2: u64;
+    let param3: u64;
     unsafe {
         asm!("mov {}, rax", out(reg) call_num);
         asm!("mov {}, rdi", out(reg) param1);
@@ -77,39 +78,60 @@ extern "x86-interrupt" fn syscall(_: InterruptStackFrame) {
 use pic8259::ChainedPics;
 
 /// Static PICS controller wrapped in a Mutex
-pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(
-    unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)});
-
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
 /// Remapped PIC 1 controller offset in the interrupt controller table
 pub const PIC_1_OFFSET: u8 = 32;
 
-
 /// Remapped PIC 2 controller offset in the interrupt controller table
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 
+pub static READY: spin::Mutex<bool> = spin::Mutex::new(false); 
 
 use x86_64::structures::idt::InterruptStackFrame;
 use x86_64::structures::idt::SelectorErrorCode;
 
-extern "x86-interrupt" fn general_protection_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    panic!("EXCEPTION: GENERAL PROTECTION FAULT\n{:#?}\nERROR CODE : {:#?}", stack_frame, SelectorErrorCode::new(error_code));
+extern "x86-interrupt" fn general_protection_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: GENERAL PROTECTION FAULT\n{:#?}\nERROR CODE : {:#?}",
+        stack_frame,
+        SelectorErrorCode::new(error_code)
+    );
 }
 
 extern "x86-interrupt" fn divide_error_handler(stack_frame: InterruptStackFrame) {
     panic!("EXCEPTION: DIVIDE ERROR\n{:#?}", stack_frame);
 }
 
-extern "x86-interrupt" fn security_exception_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    panic!("EXCEPTION: SECURITY EXCEPTION ERROR\n{:#?}\nERROR CODE : {:#?}", stack_frame, SelectorErrorCode::new(error_code));
+extern "x86-interrupt" fn security_exception_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: SECURITY EXCEPTION ERROR\n{:#?}\nERROR CODE : {:#?}",
+        stack_frame,
+        SelectorErrorCode::new(error_code)
+    );
 }
 
 extern "x86-interrupt" fn stack_segment_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    panic!("EXCEPTION: STACK SEGMENT FAULT\n{:#?}\nERROR CODE : {:#?}", stack_frame, SelectorErrorCode::new(error_code));
+    panic!(
+        "EXCEPTION: STACK SEGMENT FAULT\n{:#?}\nERROR CODE : {:#?}",
+        stack_frame,
+        SelectorErrorCode::new(error_code)
+    );
 }
 
 extern "x86-interrupt" fn invalid_tss_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    panic!("EXCEPTION: INVALID TSS\n{:#?}\nERROR CODE : {:#?}", stack_frame, SelectorErrorCode::new(error_code));
+    panic!(
+        "EXCEPTION: INVALID TSS\n{:#?}\nERROR CODE : {:#?}",
+        stack_frame,
+        SelectorErrorCode::new(error_code)
+    );
 }
 
 extern "x86-interrupt" fn invalid_op_handler(stack_frame: InterruptStackFrame) {
@@ -117,11 +139,22 @@ extern "x86-interrupt" fn invalid_op_handler(stack_frame: InterruptStackFrame) {
 }
 
 extern "x86-interrupt" fn alignment_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    panic!("EXCEPTION: OUT OF ALIGNMENT\n{:#?}\nERROR CODE : {:#?}", stack_frame, SelectorErrorCode::new(error_code));
+    panic!(
+        "EXCEPTION: OUT OF ALIGNMENT\n{:#?}\nERROR CODE : {:#?}",
+        stack_frame,
+        SelectorErrorCode::new(error_code)
+    );
 }
 
-extern "x86-interrupt" fn segment_not_present_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    panic!("EXCEPTION: SEGMENT NOT PRESENT\n{:#?}\nERROR CODE : {:#?}", stack_frame, SelectorErrorCode::new(error_code));
+extern "x86-interrupt" fn segment_not_present_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: SEGMENT NOT PRESENT\n{:#?}\nERROR CODE : {:#?}",
+        stack_frame,
+        SelectorErrorCode::new(error_code)
+    );
 }
 
 extern "x86-interrupt" fn bound_range_handler(stack_frame: InterruptStackFrame) {
@@ -146,26 +179,25 @@ extern "x86-interrupt" fn overflow_handler(stack_frame: InterruptStackFrame) {
     }
 }
 
-
 ///Doesnt do anything at the moment
 ///TODO: Notify the ata caller that the ata controller is ready
 extern "x86-interrupt" fn ata_interrupt_handler(_stack_frame: InterruptStackFrame) {
     unsafe {
-        PICS.lock().notify_end_of_interrupt(InterruptIndex::PrimATA.as_u8());
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::PrimATA.as_u8());
     }
 }
-
 
 ///Reads the key code from 0x60 port and adds that to the keyboard task handler
 extern "x86-interrupt" fn mouse_interrupt_handler(_stack_frame: InterruptStackFrame) {
     use x86_64::instructions::port::Port;
     let mut port = Port::new(0x60);
-    let scancode: u8 = unsafe{ port.read() };
+    let scancode: u8 = unsafe { port.read() };
     unsafe { mouse::add_scancode(scancode) };
-    
 
     unsafe {
-        PICS.lock().notify_end_of_interrupt(InterruptIndex::Mouse.as_u8());
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Mouse.as_u8());
     }
 }
 
@@ -173,16 +205,17 @@ extern "x86-interrupt" fn mouse_interrupt_handler(_stack_frame: InterruptStackFr
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
     use x86_64::instructions::port::Port;
     let mut port = Port::new(0x60);
-    let scancode: u8 = unsafe{ port.read() };
-    keyboard::add_scancode(scancode); 
+    let scancode: u8 = unsafe { port.read() };
+    keyboard::add_scancode(scancode);
 
     unsafe {
-        PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
 }
 
-use x86_64::structures::idt::PageFaultErrorCode;
 use x86_64::registers::control::Cr2;
+use x86_64::structures::idt::PageFaultErrorCode;
 use x86_64::structures::paging::Mapper;
 use x86_64::structures::paging::Page;
 use x86_64::structures::paging::PageTableFlags;
@@ -192,7 +225,10 @@ use x86_64::structures::paging::Size4KiB;
 use crate::syscall::SYSTEM_CALLS;
 
 ///Page fault handler prints out the respective errors and stack frame and halts cpu execution
-extern "x86-interrupt" fn page_fault_handler(_stack_frame: InterruptStackFrame, _error_code: PageFaultErrorCode) {
+extern "x86-interrupt" fn page_fault_handler(
+    _stack_frame: InterruptStackFrame,
+    _error_code: PageFaultErrorCode,
+) {
     let acc_addr = Cr2::read();
     println!("EXCEPTION: PAGE FAULT");
     println!("Accessed Address: {:?}", acc_addr);
@@ -212,30 +248,34 @@ extern "x86-interrupt" fn page_fault_handler(_stack_frame: InterruptStackFrame, 
             x86_64::instructions::hlt();
         }
     }
-    
-   
 }
-
 
 ///Used for task time slices
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    
+    println!("*");
     unsafe {
-        PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
     }
+    if *READY.lock() {
+        Scheduler::run();
+    }
+    
+
 }
 
 ///Double fault interrupt panics and prints the stack frame
-extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
-} 
+}
 
 ///Breakpoints print out the stack frame at a specified breakpoint
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
-
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame)
 }
-
 
 /// Interrupt Index enum with all of the different interrupt handler types  
 #[derive(Debug, Clone, Copy)]
@@ -259,17 +299,14 @@ pub enum InterruptIndex {
     SecoATA,
 }
 
-
 impl InterruptIndex {
-
     /// u8 representation from the PIC_1_OFFSET
     pub fn as_u8(self) -> u8 {
         self as u8
     }
-    
+
     /// usize representation from the PIC_1_OFFSET
     fn as_usize(self) -> usize {
         usize::from(self.as_u8())
     }
 }
-
--- crate/interrupts/src/syscall.rs
+++ crate/interrupts/src/syscall.rs
@@ -1,28 +1,59 @@
 use core::slice;
 
-use printer::{println, print};
+use printer::{print, println};
+
+
 
 global_asm!(include_str!("syscall_interrupts.s"));
 
 extern "C" {
-    pub fn syscall(call_num : u64, param1 : u64, param2 : u64, param3: u64) -> u64;
-    fn exit_syscall(call_num : u64) -> !;
+    pub fn syscall(call_num: u64, param1: u64, param2: u64, param3: u64) -> u64;
 }
 
+pub(crate) static SYSTEM_CALLS: [fn(param1: u64, param2: u64, param3: u64); 2] = [
+    // Syscall 0
+    print, // Syscall 1
+    exit,
+];
 
-pub(crate) static SYSTEM_CALLS : [fn(param1 : u64, param2 : u64, param3: u64); 1] = [
-        print
-    ];
-
-fn print(_file_descriptor : u64, affective_address : u64, bytes: u64) {
+fn print(_file_descriptor: u64, affective_address: u64, bytes: u64) {
     // TODO IMPLEMENT FD AND ERROR CHECKING
     unsafe {
         let slice = slice::from_raw_parts(affective_address as *const _, bytes as usize);
-        let str = core::str::from_utf8(slice).unwrap();
-        print!("{}", str);
+        match core::str::from_utf8(slice) {
+            Ok(str) => print!("{}", str),
+            Err(_) => {
+                let number = affective_address as *const u64;
+
+                print!("{}", *number)
+            }
+        }
     }
-    
-
-
 }
 
+
+/// Get the current running task from the mutex scheduler,
+/// changed the tasks state to finished then use a timer interrupt
+/// to invoke the scheduler, the scheduler will then take the current
+/// task off of a ready queue so it won't execute again
+fn exit(_: u64, _: u64, _: u64) {
+    use task::scheduler::Scheduler;
+    use task::task::{TaskState, Task};
+    use crate::InterruptIndex;
+
+    let mut scheduler = Scheduler::get_scheduler();
+    let running_task: &mut Task = scheduler
+        .running_task()
+        .unwrap_or_else(|| unreachable!());
+    
+    running_task.set_state(TaskState::Finished);
+
+    unsafe {
+        asm!(
+            "int 32"
+        );
+    }
+    // We somehow need to get the current task and change its state to finished then invoke the scheduler
+    // Should we store the current task as a mutable global? maybe
+    // then invoke the scheduler from here
+}
